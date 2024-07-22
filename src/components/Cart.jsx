e08da3ae7d1cdd041369a2f2cb853f8f778e05d0
Xavi-Arnau <chavier28@gmail.com>
2024-07-22 19:17:16 +0200

    small layout fixes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,12 @@
 import { Trees } from "lucide-react";
 import { useSelector } from "react-redux";
 import CartLine from "./CartLine";
+
 const Cart = ({ confirm }) => {
   const { cartItems } = useSelector((state) => state.cart);
   //console.log(cartItems);
   return (
-    <div className="flex flex-col gap-2 p-8 bg-white rounded-xl mt-10">
+    <div className="flex flex-col gap-2 md:p-8 bg-white rounded-xl mt-10">
       <h3 className="text-2xl font-bold text-Red">
         Your Cart ({cartItems.length || 0})
       </h3>
@@ -26,15 +27,16 @@ const Cart = ({ confirm }) => {
       </div>
       <div>
         <div className="md:w-10/12 md:mx-auto py-2 px-4 bg-Rose50 text-center flex flex-row gap-4">
-          <Trees size={22} className="text-Green" /> This is a
-          <span className="font-bold">carbon-neutral</span>
-          delivery
+          <Trees size={22} className="text-Green" />
+          <div>
+            This is a <span className="font-bold">carbon-neutral</span> delivery
+          </div>
         </div>
       </div>
       {cartItems && cartItems.length > 0 ? (
         <button
           onClick={confirm}
-          className="mt-8 bg-Red text-white py-2 px-4 w-full rounded-3xl active:bg-white active:text-Red"
+          className="mt-8 mb-8 bg-Red text-white py-2 px-4 w-full rounded-3xl active:bg-white active:text-Red"
         >
           Confirm Order
         </button>
